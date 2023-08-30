047334d8321f686a765aa782987ab1e23170ec77
MikeRubio <miguel.rubiojimenez@gmail.com>
2023-08-30 21:45:22 +0200

    fix: api prod uri

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,12 @@ import App from "./App.jsx";
 import "./index.css";
 
 const client = new ApolloClient({
-  uri: "https://invincible-api.railway.internal/graphql",
+  uri: "https://invincible-api-production.up.railway.app/graphql",
   headers: {
     "content-type": "application/json",
   },
   cache: new InMemoryCache(),
 });
-console.log(import.meta.env.API_URL);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
